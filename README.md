72c61e45ae4866dbb978621f02d138b7c3df5bd3
Matt Sieker <matt@siekernet.com>
2021-09-06 22:14:08 -0500

    Initial Commit

--- README.md
+++ README.md
@@ -1,1 +1,7 @@
 # rtk-query-codegen-nullable
+
+This is a simple demo project for how rtk-query-codegen behaves when API return types are marked as not null.
+
+## Project Info
+
+This project is mostly a boilerplate dotnet 5 web api project. 
---
+++ WeatherForecast.cs
@@ -0,0 +1,15 @@
+using System;
+
+namespace rtk_query_codegen_nullable
+{
+    public class WeatherForecast
+    {
+        public DateTime Date { get; set; }
+
+        public int TemperatureC { get; set; }
+
+        public int TemperatureF => 32 + (int)(TemperatureC / 0.5556);
+
+        public string Summary { get; set; }
+    }
+}
